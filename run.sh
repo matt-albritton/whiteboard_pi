8a6f92f4924203f63058e0cca264fae0b9303c7d
matt-albritton <matt.albritton@yale.edu>
2022-12-09 01:52:09 +0000

    added run executable

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+DISPLAY=:0 /home/mattspi/Downloads/whiteboard_pi/processing-2.2.1/processing-java --sketch=/home/mattspi/Downloads/whiteboard_pi/polargraphcontroller --output=/home/mattspi/Downloads/whiteboard_pi/processing-2.2.1/output --force --run
